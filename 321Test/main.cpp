11c29ae29604aab3c8f61c17f3fe17367e425cf9
peter992233 <csci321ga2a@cs321s-iMac-5.local>
2014-06-11 14:14:41 +1000

    Initial Commit

---
+++ 321Test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  321Test
+//
+//  Created by csci321ga2a on 11/06/2014.
+//  Copyright (c) 2014 csci321. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
